cb9f19e291cee733ced4535cae1fe98b0da2532c
Sean Wong <wotzhs@gmail.com>
2020-07-13 23:01:29 +0800

    master: done 61A

---
+++ 800/61A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string a, b, c="";
+	cin >> a >> b;
+	for (int i=0; i<a.size(); i++)
+		if (a[i] != b[i]) c+='1';
+		else c+='0';
+	cout << c;
+	return 0;
+}
